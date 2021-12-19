af08434d5daf48a7b2ea86f5c99e8109ed83feed
JinKyung Kim <kimradium@naver.com>
2021-12-19 20:25:24 +0900

    #5.5 To Do Setup

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ToDoList = () => {
+  const [toDo, setToDo] = useState("");
+
+  const onChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+  const onSubmitandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+
   return (
     <div>
-      <div>ssdfs</div>
+      <form onSubmit={onSubmitandler}>
+        <input
+          onChange={onChangeHandler}
+          value={toDo}
+          placeholder="Write a to do"
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
 };
